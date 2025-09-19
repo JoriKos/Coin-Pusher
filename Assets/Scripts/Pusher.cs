d941fd391c5418a3903bd39373b86d66025e5a0a
DOOT <jori-kos@hotmail.com>
2025-09-19 20:46:21 +0200

    Coin pusher goes back and forth now

---
+++ Assets/Scripts/Pusher.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    [SerializeField] private float _posOrigin, _posMax, _posMin, _speed;
+    [SerializeField] private Rigidbody _pusherRigidbody;
+    [SerializeField] private bool _moveForward;
+
+    private void Awake()
+    {
+        _posOrigin = transform.position.x;
+        _posMax = _posOrigin + _posMax;
+        _posMin = _posOrigin - _posMin;
+        _moveForward = true;
+    }
+
+    void FixedUpdate()
+    {
+        //_pusherRigidbody.MovePosition(Time.fixedDeltaTime * new Vector3(transform.position.x + 1, transform.position.y));
+        //_pusherRigidbody.MovePosition(Time.fixedDeltaTime * new Vector3(transform.position.x - 1, transform.position.y));
+
+        if (_moveForward)
+            _pusherRigidbody.AddForce(_speed * Time.deltaTime * Vector3.right);
+        else
+            _pusherRigidbody.AddForce(_speed * Time.deltaTime * -Vector3.right);
+
+        if (transform.position.x > _posMax)
+        {
+            _moveForward = false;
+        }
+        else
+        {
+            _moveForward = true;
+        }
+    }
+}
